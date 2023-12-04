050469aab9553257fafd212cc52d912e4f04bc67
hungtruong97 <hungtruong97@gmail.com>
2023-12-04 14:32:35 +0700

    add wizard

--- bin/index.js
+++ bin/index.js
@@ -4,7 +4,18 @@ import { Command } from "commander";
 import { readFileSync } from "fs";
 import { fileURLToPath } from "url";
 import { resolve, dirname } from "path";
-import { intro, outro } from "@clack/prompts";
+import {
+  intro,
+  outro,
+  text,
+  confirm,
+  select,
+  spinner,
+  isCancel,
+  cancel,
+} from "@clack/prompts";
+import { setTimeout as sleep } from "timers/promises";
+import { exit } from "process";
 import { underline, blue } from "kleur/colors";
 
 /**
@@ -96,16 +107,109 @@ program.command("wizard").action(async () => {
   intro(
     "Wizard for creating a profile card. Just follow instructions and you will be golden"
   );
-  // TODO: essentially do the same thing as in the `gen` command, but with step-by-step prompts
-  // instead of let user provide the options via command line.
-  // Read the clack docs
-  // Must:
-  // 1. Ask for name. User must provide nonempty string otherwise error
-  // 2. Ask for email. User must provide a nonempty and valid email otherwise error
-  // /   // 3. Confirm name and email. If user is not happy start over from 1
-  console.log("Hello world");
 
-  outro("Enjoy");
+  const validateEmailRegex = /^\S+@\S+\.\S+$/;
+  const length = 50;
+  let logo, logoLines;
+
+  const printDashedLine = (length, top = true) => {
+    const dash = "\u2500";
+    const cornerTopLeft = "\u250C";
+    const cornerTopRight = "\u2510";
+    const cornerBottomLeft = "\u2514";
+    const cornerBottomRight = "\u2518";
+    if (!top)
+      return `${cornerBottomLeft}${dash.repeat(length)}${cornerBottomRight}`;
+    return `${cornerTopLeft}${dash.repeat(length)}${cornerTopRight}`;
+  };
+
+  const printLine = (text, length, isColor = false) => {
+    const vertical = "\u2502";
+    const space = "\u0020";
+    return `${vertical}${isColor ? blue(text) : text}${space.repeat(
+      length - text.length
+    )}${vertical}\n`;
+  };
+
+  //Ask for name
+  const name = await text({
+    message: "What is your name?",
+    placeholder: "Enter your name",
+    validate(input) {
+      if (input.length === 0) return `Your name is required`;
+    },
+  });
+
+  //Ask for email
+  const email = await text({
+    message: "What is your email?",
+    placeholder: "Enter your email",
+    validate(input) {
+      if (input.length === 0) return `Your email is required`;
+      if (!validateEmailRegex.test(input))
+        return `Please enter a valid email address`;
+    },
+  });
+
+  //Ask for company
+  const selectCompany = await select({
+    message: "Which is your company?",
+    options: [
+      { value: "4D", label: "4Digit" },
+      { value: "Google", label: "Google" },
+      { value: "None", label: `I'm Batman` },
+    ],
+  });
+
+  switch (selectCompany) {
+    case "4D":
+      logo = readFileSync(resolvePath("./templates/4d.txt"), "utf-8");
+      logoLines = logo.split("\n");
+      break;
+    case "Google":
+      logo = readFileSync(resolvePath("./templates/google.txt"), "utf-8");
+      logoLines = logo.split("\n");
+      break;
+    case "None":
+      logo = readFileSync(resolvePath("./templates/bat.txt"), "utf-8");
+      logoLines = logo.split("\n");
+      break;
+  }
+
+  //Ask for confirmation
+  const confirmation = await confirm({
+    message: `\nYour name is ${name}.\n Your email is ${email}.\n Your company is ${selectCompany}.\n Do you want to continue?\n`,
+  });
+
+  if (isCancel(confirmation)) {
+    cancel("Card cancelled");
+    process.exit(0);
+  }
+
+  if (confirmation) {
+    //Create spinner
+    const s = spinner();
+    try {
+      s.start("Generating card");
+      await sleep(1000);
+      s.stop("Card generated");
+      console.log(
+        `\nHere is your card:\n` +
+          `${printDashedLine(length)}\n` +
+          logoLines.map((line) => printLine(line, length, true)).join("") +
+          `${printLine(" Name: " + name, length)}` +
+          `${printLine(" Email: " + email, length)}` +
+          `${printLine(" Company: " + (selectCompany || "N/A"), length)}` +
+          `${printDashedLine(length, false)}`
+      );
+      outro("Thanks for using our card generator");
+    } catch (err) {
+      console.log(err);
+    }
+  } else {
+    console.log("Card cancelled");
+    outro("Thanks for using our card generator");
+  }
 });
 
 program.parse(process.argv);
