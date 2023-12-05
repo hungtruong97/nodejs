15d56abcf6f2df91f91ad940e642887ddcbe7ba4
hungtruong97 <hungtruong97@gmail.com>
2023-12-05 10:03:11 +0700

    fix buddha command

--- bin/index.js
+++ bin/index.js
@@ -43,11 +43,14 @@ const pkg = loadPackageJSON();
 const program = new Command();
 program.name("pcg").description("Profile card generator").version(pkg.version);
 
-program.name("buddha").action(() => {
-  const txt = readFileSync(resolvePath("./templates/budda.txt"), "utf-8");
-  console.log("             " + underline("Buddha bless you and your code"));
-  console.log(blue(txt));
-});
+program
+  .command("buddha")
+  .description("Give you peace of mind")
+  .action(() => {
+    const txt = readFileSync(resolvePath("./templates/budda.txt"), "utf-8");
+    console.log("             " + underline("Buddha bless you and your code"));
+    console.log(blue(txt));
+  });
 
 //Generating profile card.
 program
@@ -122,112 +125,115 @@ program
   });
 
 //Create a wizard for generating profile card.
-program.command("wizard").action(async () => {
-  intro(
-    "Wizard for creating a profile card. Just follow instructions and you will be golden"
-  );
+program
+  .command("wizard")
+  .description("Step by step walk through to create a profile card.")
+  .action(async () => {
+    intro(
+      "Wizard for creating a profile card. Just follow instructions and you will be golden"
+    );
 
-  const validateEmailRegex = /^\S+@\S+\.\S+$/;
-  const length = 50;
-  let logo, logoLines;
+    const validateEmailRegex = /^\S+@\S+\.\S+$/;
+    const length = 50;
+    let logo, logoLines;
 
-  const printDashedLine = (length, top = true) => {
-    const dash = "\u2500";
-    const cornerTopLeft = "\u250C";
-    const cornerTopRight = "\u2510";
-    const cornerBottomLeft = "\u2514";
-    const cornerBottomRight = "\u2518";
-    if (!top)
-      return `${cornerBottomLeft}${dash.repeat(length)}${cornerBottomRight}`;
-    return `${cornerTopLeft}${dash.repeat(length)}${cornerTopRight}`;
-  };
+    const printDashedLine = (length, top = true) => {
+      const dash = "\u2500";
+      const cornerTopLeft = "\u250C";
+      const cornerTopRight = "\u2510";
+      const cornerBottomLeft = "\u2514";
+      const cornerBottomRight = "\u2518";
+      if (!top)
+        return `${cornerBottomLeft}${dash.repeat(length)}${cornerBottomRight}`;
+      return `${cornerTopLeft}${dash.repeat(length)}${cornerTopRight}`;
+    };
 
-  const printLine = (text, length, isColor = false) => {
-    const vertical = "\u2502";
-    const space = "\u0020";
-    return `${vertical}${isColor ? blue(text) : text}${space.repeat(
-      length - text.length
-    )}${vertical}\n`;
-  };
+    const printLine = (text, length, isColor = false) => {
+      const vertical = "\u2502";
+      const space = "\u0020";
+      return `${vertical}${isColor ? blue(text) : text}${space.repeat(
+        length - text.length
+      )}${vertical}\n`;
+    };
 
-  let isConfirmed;
-  while (isConfirmed === false || isConfirmed === undefined) {
-    //Ask for name
-    const name = await text({
-      message: "What is your name?",
-      placeholder: "Enter your name",
-      validate(input) {
-        if (input.length === 0) return `Your name is required`;
-      },
-    });
+    let isConfirmed;
+    while (isConfirmed === false || isConfirmed === undefined) {
+      //Ask for name
+      const name = await text({
+        message: "What is your name?",
+        placeholder: "Enter your name",
+        validate(input) {
+          if (input.length === 0) return `Your name is required`;
+        },
+      });
 
-    //Ask for email
-    const email = await text({
-      message: "What is your email?",
-      placeholder: "Enter your email",
-      validate(input) {
-        if (input.length === 0) return `Your email is required`;
-        if (!validateEmailRegex.test(input))
-          return `Please enter a valid email address`;
-      },
-    });
+      //Ask for email
+      const email = await text({
+        message: "What is your email?",
+        placeholder: "Enter your email",
+        validate(input) {
+          if (input.length === 0) return `Your email is required`;
+          if (!validateEmailRegex.test(input))
+            return `Please enter a valid email address`;
+        },
+      });
 
-    //Ask for company
-    const selectCompany = await select({
-      message: "Which is your company?",
-      options: [
-        { value: "4D", label: "4Digit" },
-        { value: "Google", label: "Google" },
-        { value: "None", label: `I'm Batman` },
-      ],
-    });
+      //Ask for company
+      const selectCompany = await select({
+        message: "Which is your company?",
+        options: [
+          { value: "4D", label: "4Digit" },
+          { value: "Google", label: "Google" },
+          { value: "None", label: `I'm Batman` },
+        ],
+      });
 
-    switch (selectCompany) {
-      case "4D":
-        logo = readFileSync(resolvePath("./templates/4d.txt"), "utf-8");
-        logoLines = logo.split("\n");
-        break;
-      case "Google":
-        logo = readFileSync(resolvePath("./templates/google.txt"), "utf-8");
-        logoLines = logo.split("\n");
-        break;
-      case "None":
-        logo = readFileSync(resolvePath("./templates/bat.txt"), "utf-8");
-        logoLines = logo.split("\n");
-        break;
-    }
-
-    //Ask for confirmation
-    const confirmation = await confirm({
-      message: `\nYour name is ${name}.\n Your email is ${email}.\n Your company is ${selectCompany}.\n Do you want to continue?\n`,
-    });
-
-    if (confirmation) {
-      //Create spinner
-      const s = spinner();
-      try {
-        s.start("Generating card");
-        await sleep(1000);
-        s.stop("Card generated");
-        console.log(
-          `\nHere is your card:\n` +
-            `${printDashedLine(length)}\n` +
-            logoLines.map((line) => printLine(line, length, true)).join("") +
-            `${printLine(" Name: " + name, length)}` +
-            `${printLine(" Email: " + email, length)}` +
-            `${printLine(" Company: " + (selectCompany || "N/A"), length)}` +
-            `${printDashedLine(length, false)}`
-        );
-        outro("Thanks for using our card generator");
-        isConfirmed = true;
-      } catch (err) {
-        console.log(err);
+      switch (selectCompany) {
+        case "4D":
+          logo = readFileSync(resolvePath("./templates/4d.txt"), "utf-8");
+          logoLines = logo.split("\n");
+          break;
+        case "Google":
+          logo = readFileSync(resolvePath("./templates/google.txt"), "utf-8");
+          logoLines = logo.split("\n");
+          break;
+        case "None":
+          logo = readFileSync(resolvePath("./templates/bat.txt"), "utf-8");
+          logoLines = logo.split("\n");
+          break;
       }
-    } else {
-      // console.log("Card cancelled");
-      // outro("Thanks for using our card generator");
+
+      //Ask for confirmation
+      const confirmation = await confirm({
+        message: `\nYour name is ${name}.\n Your email is ${email}.\n Your company is ${selectCompany}.\n Do you want to continue?\n`,
+      });
+
+      if (confirmation) {
+        //Create spinner
+        const s = spinner();
+        try {
+          s.start("Generating card");
+          await sleep(1000);
+          s.stop("Card generated");
+          console.log(
+            `\nHere is your card:\n` +
+              `${printDashedLine(length)}\n` +
+              logoLines.map((line) => printLine(line, length, true)).join("") +
+              `${printLine(" Name: " + name, length)}` +
+              `${printLine(" Email: " + email, length)}` +
+              `${printLine(" Company: " + (selectCompany || "N/A"), length)}` +
+              `${printDashedLine(length, false)}`
+          );
+          outro("Thanks for using our card generator");
+          isConfirmed = true;
+        } catch (err) {
+          console.log(err);
+        }
+      } else {
+        // console.log("Card cancelled");
+        // outro("Thanks for using our card generator");
+      }
     }
-  }
-});
+  });
 
 program.parse(process.argv);
